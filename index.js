84dd2eca96d2eac375285c12e6862f5d0f408460
Zaiyebuzuoyouqingdetiangou <xinyuezhang847@gmail.com>
2026-07-08 12:14:59 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import { initRabbitHoleUI } from './src/ui.js';
 import { rabbitHoleGenerateInterceptor, clearRabbitHolePrompt } from './src/injector.js';
 import { clearLastCombo } from './src/storage.js';
 import { initVisualScanner } from './src/visualScanner.js';
+import { initOutputSanitizer } from './src/outputSanitizer.js';
 
 // SillyTavern reads this global function name from manifest.json -> generate_interceptor.
 globalThis.rabbitHoleGenerateInterceptor = rabbitHoleGenerateInterceptor;
 
 jQuery(async () => {
     initRabbitHoleUI();
+    initOutputSanitizer();
     initVisualScanner();
     console.log('[RabbitHole] loaded');
 });
