fc92229e7ec31cd0c80d5d7cb11d71c9dbdad855
Zaiyebuzuoyouqingdetiangou <xinyuezhang847@gmail.com>
2026-07-22 10:14:24 +0900

    Add files via upload

--- data/raw/rawSegmentLookup.js
+++ data/raw/rawSegmentLookup.js
@@ -1,5 +1,5 @@
-import { RAW_THEMATIC_CATEGORIES } from './rawThematicCategories.js?rmv=0.33.42';
-import { RAW_PRESENTATION_FORMATS } from './rawPresentationFormats.js?rmv=0.33.42';
+import { RAW_THEMATIC_CATEGORIES } from './rawThematicCategories.js?rmv=0.33.44';
+import { RAW_PRESENTATION_FORMATS } from './rawPresentationFormats.js?rmv=0.33.44';
 
 function escapeRegExp(text) {
     return String(text).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
