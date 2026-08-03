80b94d3dc8d9f044b978e753abcc6b3cd1ff9241
Zaiyebuzuoyouqingdetiangou <xinyuezhang847@gmail.com>
2026-08-03 16:53:40 +0900

    Add files via upload

--- data/raw/rawSegmentLookup.js
+++ data/raw/rawSegmentLookup.js
@@ -1,5 +1,5 @@
-import { RAW_THEMATIC_CATEGORIES } from './rawThematicCategories.js?rmv=1.2.13';
-import { RAW_PRESENTATION_FORMATS } from './rawPresentationFormats.js?rmv=1.2.13';
+import { RAW_THEMATIC_CATEGORIES } from './rawThematicCategories.js?rmv=1.2.14';
+import { RAW_PRESENTATION_FORMATS } from './rawPresentationFormats.js?rmv=1.2.14';
 
 function escapeRegExp(text) {
     return String(text).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
